9f338cc462c59362cade41aee229d8f0c2aaa961
David <david25042001@gmail.com>
2023-12-20 10:49:05 +0100

    creacion del proyecto

---
+++ juegosdelhambre/src/main/java/examenrecu/example/juegosdelhambre/JuegosdelhambreApplication.java
@@ -0,0 +1,13 @@
+package examenrecu.example.juegosdelhambre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegosdelhambreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegosdelhambreApplication.class, args);
+	}
+
+}
---
+++ juegosdelhambre/src/main/java/examenrecu/example/juegosdelhambre/ServletInitializer.java
@@ -0,0 +1,13 @@
+package examenrecu.example.juegosdelhambre;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JuegosdelhambreApplication.class);
+	}
+
+}
