8645598cb92c13af5148714105eb15008450426a
sylvya0 <lazaro.rosil22@zaragoza.salesianos.edu>
2024-01-25 17:36:44 +0100

    quitadosimportsnousados

--- CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/RefrescoController.java
+++ CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/RefrescoController.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import JuegosdelhambreDDI.CafeteriaAPP.model.Cafe;
 import JuegosdelhambreDDI.CafeteriaAPP.model.Refresco;
 import JuegosdelhambreDDI.CafeteriaAPP.service.RefrescoService;
 
