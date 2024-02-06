71aa5630d8d8c223fd683d4028d35b465f3bb820
Kanutos <bielsa.arign22@zaragoza.salesianos.edu>
2024-02-06 18:07:17 +0100

    "Modificacion de Login,Register e Index"

--- CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/UsuarioController.java
+++ CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/UsuarioController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import JuegosdelhambreDDI.CafeteriaAPP.model.Usuario;
 import JuegosdelhambreDDI.CafeteriaAPP.service.UsuarioService;
@@ -23,9 +26,25 @@ public class UsuarioController {
 	}
 	
 	@RequestMapping("/Login")
-	public String login() {
+	public String login() {		
 		
 		return "/Login";
+	}
+	
+	@PostMapping("/ValidarLogin")
+	    public ModelAndView login(@RequestParam String email, @RequestParam String password) {
+	        Usuario usuario = usuarioService.searchUsuariobyEmail(email);
+	        if (usuario != null && usuario.getPassword().equals(password)) {
+	            return new ModelAndView("/index");
+	        } else {
+	            return new ModelAndView("/Login");
+	        }
+	    }
+	
+	@RequestMapping("/index")
+	public String index(String password) {
+		
+		return "/index";
 	}
 	
 	
