d40a55ccc57a7477a41b9d7d08565ef5f780f0e5
sylvya0 <lazaro.rosil22@zaragoza.salesianos.edu>
2024-02-08 12:50:43 +0100

    borrador de trabajador controller

--- CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/TrabajadorController.java
+++ CafeteriaAPP/CafeteriaAPP/src/main/java/JuegosdelhambreDDI/CafeteriaAPP/controllers/TrabajadorController.java
@@ -16,5 +16,56 @@ import JuegosdelhambreDDI.CafeteriaAPP.service.PedidoService;
 
 
 public class TrabajadorController {
+
+
+    @Autowired
+    private TrabajadorService trabajadorService;
+    @Autowired
+    private PedidoService pedidoService;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping("/trabajadores")
+    public String getTrabajadores(Model model) {
+        List<Trabajador> trabajadores = trabajadorService.getAllTrabajador();
+        model.addAttribute("trabajadores", trabajadores);
+        return "trabajadores";
+    }
+
+    @RequestMapping("/pedidos")
+    public String getPedidos(Model model) {
+        List<Pedido> pedidos = pedidoService.getAllPedidos();
+        model.addAttribute("pedidos", pedidos);
+        return "pedidos";
+    }
+
+   /*  @RequestMapping("/pedidos/pendientes")
+    public String getPedidosPendientes(Model model) {
+        List<Pedido> pedidos = pedidoService.getPedidosPendientes();
+        model.addAttribute("pedidos", pedidos);
+        return "pedidos";
+    }
+
+
+    */
+
+    @RequestMapping("/pedidos/eliminar")
+    public String eliminarPedido(Model model, int id) {
+        pedidoService.deletePedido(id);;
+        return "redirect:/pedidos/pendientes";
+    }
+
+    @RequestMapping("/pedidos/crear")
+    public String crearPedido(Model model, Pedido pedido) {
+        pedidoService.addPedido(pedido);
+        return "redirect:/pedidos/pendientes";
+    }
+
+   /* @RequestMapping("/pedidos/editar")
+    public String editarPedido(Model model, Pedido pedido) {
+        pedidoService.editarPedido(pedido);
+        return "redirect:/pedidos/pendientes";
+    }*/
+    
     
 }
